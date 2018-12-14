1498becea556e8d5647290cc93c0c85de1887b26
morningbell <brianna124@gmail.com>
2018-12-13 22:08:46 -0500

    Added some strings, integers, and booleans

--- hello.go
+++ hello.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
   fmt.Println("Hello, World!")
+
+  fmt.Println("Bri" + "anna")
+
+  fmt.Println("1+1=", 1+1)
+
+  fmt.Println(true && false)
+  fmt.Println(true || false)
 }
