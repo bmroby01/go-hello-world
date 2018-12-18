db96d583f8cc858210568b83bab62acaaeb88f72
morningbell <brianna124@gmail.com>
2018-12-17 19:40:04 -0500

    Working with Go variables

--- hello.go
+++ hello.go
@@ -11,4 +11,19 @@ func main() {
 
   fmt.Println(true && false)
   fmt.Println(true || false)
+
+  var a = "initial"
+  fmt.Println(a)
+
+  var b, c int = 1, 2
+  fmt.Println(b, c)
+
+  var d = true
+  fmt.Println(d)
+
+  var e int
+  fmt.Println(e)
+
+  f := "short"
+  fmt.Println(f)
 }
